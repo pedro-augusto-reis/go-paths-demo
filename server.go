275c308defd6148d473bc5e43fcc062708d43cac
Pedro Reis <pdroreis@gmail.com>
2023-03-22 20:51:55 -0300

    ajuste cluster sao paulo

--- server.go
+++ server.go
@@ -7,13 +7,13 @@ func main() {
 	http.HandleFunc("/sistema-a", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("<h1>Alltech Soluções em Tecnologia.</h1>\n " +
 			"<h2>Bem vindo ao sistema A.</h2>\n" +
-			"<h3>Cluster Brasília</h3>"))
+			"<h3>Cluster São Paulo</h3>"))
 	})
 
 	http.HandleFunc("/sistema-b", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("<h1>Alltech Soluções em Tecnologia.</h1>\n " +
 			"<h2>Bem vindo ao sistema B.</h2>\n" +
-			"<h3>Cluster Brasília</h3>"))
+			"<h3>Cluster São Paulo</h3>"))
 	})
 	http.ListenAndServe(":8080", nil)
 }
