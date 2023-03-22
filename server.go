f55881f8c92276132b9063f37313cb5ce88c0e92
Pedro Reis <pdroreis@gmail.com>
2023-03-22 20:51:13 -0300

    ajuste cluster brasilia

--- server.go
+++ server.go
@@ -5,11 +5,15 @@ import "net/http"
 func main() {
 
 	http.HandleFunc("/sistema-a", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("<h1>Alltech Soluções em Tecnologia.</h1>\n <h2>Bem vindo ao sistema A.</h2>"))
+		w.Write([]byte("<h1>Alltech Soluções em Tecnologia.</h1>\n " +
+			"<h2>Bem vindo ao sistema A.</h2>\n" +
+			"<h3>Cluster Brasília</h3>"))
 	})
 
 	http.HandleFunc("/sistema-b", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("<h1>Alltech Soluções em Tecnologia.</h1>\n <h2>Bem vindo ao sistema B.</h2>"))
+		w.Write([]byte("<h1>Alltech Soluções em Tecnologia.</h1>\n " +
+			"<h2>Bem vindo ao sistema B.</h2>\n" +
+			"<h3>Cluster Brasília</h3>"))
 	})
 	http.ListenAndServe(":8080", nil)
 }
